c4f15142193c8efc15484ba993f54f6135b7c4bf
Mikkel Kaas <C:\Users\m\AppData\Roaming\The Bat!>
2021-06-11 16:09:29 +0100

    Site updated

---
+++ pushBuild.bat
@@ -0,0 +1,10 @@
+@echo off
+echo "Updating git repo"
+git add *
+git commit -m "Site updated"
+git push
+echo "Rebuilding docker image"
+docker build -t openlabdk/thefatcyclist:v1 .
+docker push
+echo "Done."
+echo "(Within 60 min. all new updates are public)"
