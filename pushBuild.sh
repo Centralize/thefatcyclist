814547fa5de3f7d857d5873f29da886591cd1d07
Mikkel Kaas <mkaas@openlab.dk>
2021-06-13 15:59:58 +0000

    Site updated

--- pushBuild.sh
+++ pushBuild.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-echo "Updating git repository."
+echo "Updating git repo"
 git add *
-git commit -m "Site Updated"
+git commit -m "Site updated"
 git push
+
 echo "Rebuilding docker image"
 docker build -t openlabdk/thefatcyclist:v1 .
 docker push openlabdk/thefatcyclist:v1
